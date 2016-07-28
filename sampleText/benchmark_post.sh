2f4d884d88cf218bdb5b4a353c02221bcac268b4
Iwan Winoto <iwinoto@gmail.com>
2016-07-29 08:47:28 +1000

    bench scripts

--- sampleText/benchmark_post.sh
+++ sampleText/benchmark_post.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
-JSON=Bill_Shorten-02.json
-#JSON="Bill\ Shorten-02.json"
+JSON=Bill_Shorten.json
 CMD="ab -n 1 -c 1 -r -v 4 -p ${JSON} -T 'application/json' http://iw-personalityinsights-demo.mybluemix.net/api/profile"
 
 echo $CMD
